0e079178fcdf5d2408a85b730a0549738c333717
ahare14 <alexanderhare14@gmail.com>
2019-06-03 16:46:58 -0600

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    return 'This one is on me!'
+  } else if (ride > 2000 && ride < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let decision;
+  city === 'NYC' ? (decision = 'Ok, sounds good.') : (decision = 'No go.');
+  return decision
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response
+  switch (tip) {
+    case 'generous' :
+      response = 'Thank you so much.'
+      return response
+    case 'not as generous' :
+      response = 'Thank you.'
+      return response
+    default:
+      response = 'Bye.'
+      return response
+  }
+
 }
